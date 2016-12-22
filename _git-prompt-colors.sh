8170834e227237f5c33cbb2c0fd2eff02526eba9
Denny Gebel <nomoresecrets@gmail.com>
2016-12-22 12:45:44 +0100

    tweaking prompt

--- _git-prompt-colors.sh
+++ _git-prompt-colors.sh
@@ -1,7 +1,6 @@
 override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="Custom"
-  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_${ResetColor}[${BoldRed}${USER}${ResetColor}@${Green}${HOSTNAME%%.*}${ResetColor}:${Yellow}${PathShort}${ResetColor}]"
-
+  GIT_PROMPT_START_USER="${BoldRed}${USER}${ResetColor}@${Green}${HOSTNAME%%.*}:${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_START_ROOT="${BoldRed}${USER}${ResetColor}@${Green}${HOSTNAME%%.*}:${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_END_ROOT="#${ResetColor} "
   GIT_PROMPT_END_USER="#${ResetColor} "
